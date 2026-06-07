aeb1999bc3eee656e47870f0c6fbabfe6799b260
DEVIL-enc <moahmedjujuj@gmail.com>
2026-06-07 04:41:46 +0200

    Update database.py

--- database.py
+++ database.py
@@ -4,8 +4,9 @@ import datetime
 from motor.motor_asyncio import AsyncIOMotorClient
 
 # MongoDB Connection
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
-DB_NAME = os.getenv("DB_NAME", "razor_x_bot")
+MONGO_URL = "mongodb+srv://aljylmhmd2_db_user:PASSWORD@chkdevip.vnvsptt.mongodb.net/?retryWrites=true&w=majority&appName=Chkdevip"
+DB_NAME = "razor_x_bot"
+
 
 client = AsyncIOMotorClient(MONGO_URL)
 db = client[DB_NAME]
